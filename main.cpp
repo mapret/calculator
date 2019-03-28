780aefe656fe8b583c0b01863b947e25a135a1a2
mapret <mapret-dev@outlook.com>
2019-03-28 15:31:30 +0100

    Created separate test executable

--- main.cpp
+++ main.cpp
@@ -1,59 +1,6 @@
 #include "Evaluator.hpp"
-#include <cmath>
 #include <iostream>
-#include <map>
 
-
-#define TEST(expr, result) \
-{ \
-  float res; \
-  if (Evaluator().evaluate(expr, res) && approximatelyEqual(res, static_cast<float>(result))) \
-    correct++; \
-  else \
-    wrong++; \
-}
-
-template <typename T>
-bool approximatelyEqual(T a, T b, T epsilon = T(10.e-4))
-{
-  return std::abs(a - b) <= std::max(std::abs(a), std::abs(b)) * epsilon;
-}
-
-int test()
-{
-  unsigned correct = 0;
-  unsigned wrong = 0;
-
-  TEST("3", 3)
-  TEST("2+3", 5)
-  TEST("2+2*2", 6)
-  TEST("2*(2+2)", 8)
-  TEST("(2+2)*(3+3)", 24)
-  TEST("5-3-6", -4)
-  TEST("(((2)))", 2)
-  TEST("(((2*(2+2))))", 8)
-  TEST("-2", -2)
-  TEST("-2+3", 1)
-  TEST("-(1+2)", -3)
-  TEST("2+-3", -1)
-  TEST("2--3", 5)
-  TEST("2+++3", 5)
-  TEST("2+-+-+-3", -1)
-  TEST("--2", 2)
-  TEST("2^10", 1024)
-  TEST("2+2*2^2", 10)
-  TEST("2^3^4", 4096)
-  TEST("2^-3", 0.125)
-  TEST("cos(1)", 0.5403023058681398)
-  TEST("sin(asin(0.3))", 0.3)
-  TEST("sin(0.3)^2+cos(0.3)^2", 1)
-  TEST("sin(0.2)/cos(0.2)-tan(0.2)", 0)
-  TEST("sin(PI)", 0)
-  TEST("ln(E)", 1)
-
-  std::cout << "Correct/wrong " << correct << "/" << wrong << "\n";
-  return wrong != 0;
-}
 
 int interactive()
 {
@@ -72,10 +19,6 @@ int main(int argc, char** argv)
 {
   if (argc == 1)
     return interactive();
-
-  std::string mode(argv[1]);
-  if (mode == "test")
-    return test();
 
   return 1;
 }
---
+++ test/main.cpp
@@ -0,0 +1,52 @@
+#include "Evaluator.hpp"
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+
+
+namespace fs = std::filesystem;
+
+template <typename T>
+bool approximatelyEqual(T a, T b, T epsilon = T(10.e-4))
+{
+  return std::abs(a - b) <= std::max(std::max(std::abs(a), std::abs(b)), epsilon) * epsilon;
+}
+
+int main(int argc, const char** argv)
+{
+  std::vector<std::pair<std::string, std::string>> testcases;
+  for (const auto& direntry : fs::directory_iterator("test/data"))
+  {
+    std::ifstream in(direntry.path());
+    std::string input, reference, dummy;
+    while (!in.eof())
+    {
+      std::getline(in, input);
+      std::getline(in, reference);
+      std::getline(in, dummy);
+      testcases.emplace_back(input, reference);
+    }
+  }
+
+  int num_correct = 0;
+  int num_wrong = 0;
+
+  for (const auto& testcase : testcases)
+  {
+    float reference = std::stof(testcase.second);
+    float retval;
+    if (Evaluator().evaluate(testcase.first, retval) && approximatelyEqual(reference, retval))
+    {
+      num_correct++;
+    }
+    else
+    {
+      num_wrong++;
+      std::cout << "FAIL: " << testcase.first << " was " << retval << " but should be " << reference << std::endl;
+    }
+  }
+
+  std::cout << "Num correct/wrong: " << num_correct << "/" << num_wrong << std::endl;
+
+  return num_wrong != 0;
+}
